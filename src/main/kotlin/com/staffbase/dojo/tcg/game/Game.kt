c929d1327287ad2be10a755610f7c6fa3482962d
Felix Bytow <felix@staffbase.com>
2018-11-07 20:11:31 +0100

    :sparkles: refill mana slots on game ticks

--- src/main/kotlin/com/staffbase/dojo/tcg/game/Game.kt
+++ src/main/kotlin/com/staffbase/dojo/tcg/game/Game.kt
@@ -37,11 +37,9 @@ class Game(
   }
 
   fun startTurn(player: Int) {
-    // The active player draws a random card from his deck
     players[player].addManaSlot()
+    players[player].refillMana()
     players[player].draw(1)
-    // TODO: The active player receives 1 Mana slot up to a maximum of 10 total slots
-    // TODO: The active player’s empty Mana slots are refilled
   }
 
   private fun playerCanAct(player: Int): Boolean {
